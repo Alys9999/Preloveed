15a849c13fa8e869470adf83bd7cb1f50f32c90d
HLZERONE <lhe15@uci.edu>
2023-05-14 16:05:02 -0700

    connect to back4pp(backend database)'s API

--- Preloved/src/app/app.component.ts
+++ Preloved/src/app/app.component.ts
@@ -14,5 +14,7 @@ export class AppComponent {
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor() {
+
+  }
 }
--- Preloved/src/app/folder/folder.page.ts
+++ Preloved/src/app/folder/folder.page.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import Parse from 'parse'
+import {Platform} from "@ionic/angular";
 
 @Component({
   selector: 'app-folder',
@@ -9,7 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  result: string = "What";
+  constructor(private platform: Platform) {
+    Parse.serverURL = 'https://parseapi.back4app.com/';
+    Parse.initialize("L1FFfpsxK7K1wvaTujWJUASQjfvBz5LNUDdPvK5E", "wIgCTMul7e9DrhNlU0oEUW0mlSml2xF05gB0n0BI");
+    
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
