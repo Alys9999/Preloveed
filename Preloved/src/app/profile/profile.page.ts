a4d5e936cc6f855c2037ce6005a25d2adcefcb06
Alys9999 <74774427+Alys9999@users.noreply.github.com>
2023-06-05 08:58:43 -0700

    ?

--- Preloved/src/app/profile/profile.page.ts
+++ Preloved/src/app/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {GenerateData} from '../../info/generateData';
+import { User } from 'src/info/User';
+import Parse from "parse";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  constructor() { }
+user: User | undefined;
+name: string | undefined;
+bio: string | undefined;
+email: string | undefined;
+phoneNumber: string | undefined;  
+  constructor() {
+    Parse.serverURL = 'https://parseapi.back4app.com/';
+    Parse.initialize("L1FFfpsxK7K1wvaTujWJUASQjfvBz5LNUDdPvK5E", "wIgCTMul7e9DrhNlU0oEUW0mlSml2xF05gB0n0BI");
+   }
 
   ngOnInit() {
-  }
+    this.user = GenerateData.getCurrentUser();
+    this.name = this.user.username;
+    this.bio = this.user.biography;
+    this.email = this.user.email;
+    this.phoneNumber = this.user.phoneNumber;
+  } 
 
 }
